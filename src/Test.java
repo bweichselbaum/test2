43e40b201726827c77cce4a183b04584ca0189f8
bweichselbaum <b.weichselbaum@7fun.de>
2018-10-29 20:32:24 +0100

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,11 @@
-public class Test {
+public class Test extends javax.swing.JFrame{
 
+    public static void main(String[] args){
+        Test t = new Test(); 
+    }
     
+    Test(){
+        this.setSize(500, 500);
+        this.setVisible(true);
+    }
 }
